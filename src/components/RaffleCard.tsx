c476d07d8912efcbe537b892fae67b6a37d337d5
Tcadle434 <thomascadle434@gmail.com>
2023-02-26 17:47:54 -0700

    created raffle card component and made loading a bit better

---
+++ src/components/RaffleCard.tsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+
+interface CardProps {
+  imageUrl: string;
+  nftName: string;
+  nftCollectionName: string;
+  raffleTimeRemaining: string;
+  ticketPrice: number;
+  ticketsRemaining: number;
+  totalTickets: number;
+  newLimit: () => void;
+  isLast: boolean;
+}
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const RaffleCard = ({
+  imageUrl,
+  nftName,
+  nftCollectionName,
+  raffleTimeRemaining,
+  ticketPrice,
+  ticketsRemaining,
+  totalTickets,
+  newLimit,
+  isLast,
+}: CardProps) => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const cardRef = useRef();
+
+  useEffect(() => {
+    if (!cardRef?.current) return;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (isLast && entry!.isIntersecting) {
+        newLimit();
+        observer.unobserve(entry!.target);
+      }
+    });
+
+    observer.observe(cardRef.current);
+  }, [isLast]);
+
+  return (
+    <div className="max-w-sm rounded-lg border border-gray-700 bg-secondary shadow">
+      <a href="#">
+        {/* <img className="rounded-t-lg" src={imageUrl} alt="" /> */}
+        <div className="">
+          {!isImageLoaded && (
+            <div className="">
+              <Image src="/rings.svg" alt="loader" width={200} height={200} />
+            </div>
+          )}
+          <img
+            src={imageUrl}
+            alt="user NFT"
+            className="rounded-t-lg"
+            onLoad={() => setIsImageLoaded(true)}
+            loading="lazy"
+          />
+        </div>
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className=" text-2xl font-bold tracking-tight text-light">
+            {nftCollectionName}
+          </h5>
+        </a>
+        <p className="mt mb-3 font-normal text-white">{nftName}</p>
+        <div className="mt-4 flex flex-row justify-between">
+          <div className="flex flex-col ">
+            <label className="text-md text-light">Tickets Remaining</label>
+            <p className="mb-3 font-normal text-white">
+              {ticketsRemaining} / {totalTickets}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <label className="text-md text-light">Ticket Price</label>
+            <p className="mb-3 font-normal text-white">{ticketPrice} $MATIC</p>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <label className="text-md text-light">Time Remaining</label>
+          <p className="mb-3 font-normal text-white">{raffleTimeRemaining}</p>
+        </div>
+
+        <button className="inline-flex w-full items-center justify-center rounded-lg bg-[#E58247] px-3 py-2 text-center text-sm font-medium text-white hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300">
+          View Raffle
+          <svg
+            aria-hidden="true"
+            className="ml-2 -mr-1 h-4 w-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RaffleCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 
+import RaffleCard from "~/components/RaffleCard";
 import Navbar from "~/components/Navbar";
 import useInfiniteScroll from "../hooks/useInfiniteScroll";
 
@@ -82,13 +83,30 @@ type Props = {
 };
 
 const Home = ({ loadMore }: Props) => {
-  const [images, setImages] = useState(tempImgUrls.slice(0, 50));
-  const { scrollRef, loading } = useInfiniteScroll(loadMore);
+  // const [images, setImages] = useState(tempImgUrls.slice(0, 50));
+  const [images, setImages] = useState<string[] | never[]>([]);
+  const [page, setPage] = useState(1);
 
-  const handleLoadMore = async () => {
-    await loadMore();
-    setImages(images.slice(0, images.length + 50));
+  // const { scrollRef, loading } = useInfiniteScroll(loadMore);
+
+  // const handleLoadMore = async () => {
+  //   await loadMore();
+  //   setImages(images.slice(0, images.length + 50));
+  // };
+  const fetchImages = async () => {
+    // const response = await fetch(`${BASE_URL}?query=tea&page=${page}`, {
+    //   headers: {
+    //     Authorization: `Client-ID ${process.env.NEXT_PUBLIC_UNSPLASH}`,
+    //   },
+    // });
+    // const { results } = await response.json();
+    const results = tempImgUrls.slice(0, 50);
+    setImages((prev) => [...prev, ...results]);
   };
+
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
 
   return (
     <>
@@ -102,11 +120,23 @@ const Home = ({ loadMore }: Props) => {
         <div className=" gap-12 px-4 py-16 ">
           <ul
             role="list"
-            className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+            className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
           >
-            {images.map((image) => (
+            {images.map((image, index) => (
               <li key={image} className="relative">
-                <div className="group aspect-w-8 aspect-h-8 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                <RaffleCard
+                  imageUrl={image}
+                  nftName="DeDog #44"
+                  nftCollectionName="DeDogs"
+                  raffleTimeRemaining="2 days 3 hours 5 minutes"
+                  ticketPrice={1}
+                  ticketsRemaining={2}
+                  totalTickets={10}
+                  isLast={index === images.length - 1}
+                  newLimit={() => setPage(page + 1)}
+                />
+
+                {/* <div className="group aspect-w-8 aspect-h-8 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
                   <Image
                     src={image}
                     height={150}
@@ -118,7 +148,7 @@ const Home = ({ loadMore }: Props) => {
                     type="button"
                     className="absolute inset-0 focus:outline-none"
                   >
-                    {/* <span className="sr-only">View details for {file.title}</span> */}
+                    <span className="sr-only">View details for {file.title}</span>
                   </button>
                 </div>
                 <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
@@ -126,15 +156,9 @@ const Home = ({ loadMore }: Props) => {
                 </p>
                 <p className="pointer-events-none block text-sm font-medium text-gray-500">
                   tmp
-                </p>
+                </p> */}
               </li>
             ))}
-            {loading && (
-              <Image src="/rings.svg" height={50} width={50} alt="" />
-            )}
-            <div ref={scrollRef} className="invisible">
-              &nbsp;
-            </div>
           </ul>
         </div>
       </main>
