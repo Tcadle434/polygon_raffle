106cf068266ea8f0a6e1c5117383de483d821b5b
Tcadle434 <thomascadle434@gmail.com>
2023-03-26 19:54:58 -0600

    reading the winner from the contract

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = "0x18bDED3E3ba31F720A5a020d447afb185C6197eE";
+export const CONTRACT_ADDRESS = "0x133661fF1d47c6C51000479666FEa1168484110D";
 export const BASE_EXPLORER_URL = "https://polygonscan.com";
 export const API_KEY = "ZuSU4sqkiZWPY9G2jJuWNSBu7WlqaVmK";
 export const BASE_ME_COLLECTION_URL =
