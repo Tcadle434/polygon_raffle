c1dd498ae98fb8f67c5ba6814d9d61da432ec9f2
Tcadle434 <thomascadle434@gmail.com>
2023-03-27 23:16:03 -0600

    new contract

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = "0x1dB6060C700190cB4Dd925d60Ce82b60cC8e8343";
+export const CONTRACT_ADDRESS = "0xb82BC52Ec9022A3240dD6d9782689e6C70cc4BbA";
 export const BASE_EXPLORER_URL = "https://polygonscan.com";
 export const API_KEY = "ZuSU4sqkiZWPY9G2jJuWNSBu7WlqaVmK";
 export const BASE_ME_COLLECTION_URL =
