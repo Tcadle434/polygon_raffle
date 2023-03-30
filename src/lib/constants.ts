35781268bd191126f43cbada600f99c6edb22c69
Tcadle434 <thomascadle434@gmail.com>
2023-03-30 00:43:46 -0600

    ui changed to use the emergency function if setwinner has called and chainlink failed. Still need to update with new contract abi

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = "0xDE36173B7dCDAEcd96597B2f3083EDbD57Be3deb";
+export const CONTRACT_ADDRESS = "0xcdcd3Ab4Ff1afE76F2CDE9F901251d394a96D666";
 export const BASE_EXPLORER_URL = "https://polygonscan.com";
 export const API_KEY = "ZuSU4sqkiZWPY9G2jJuWNSBu7WlqaVmK";
 export const BASE_ME_COLLECTION_URL =
