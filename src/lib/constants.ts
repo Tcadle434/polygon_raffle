036c7c8bf98e1c001fdb8d6f2133886daf712399
Tcadle434 <thomascadle434@gmail.com>
2023-03-29 23:31:13 -0600

    newest chainlink contract

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = "0xaedcB0C60e9B70758E1a236DfC263a0830D5f53C";
+export const CONTRACT_ADDRESS = "0xDE36173B7dCDAEcd96597B2f3083EDbD57Be3deb";
 export const BASE_EXPLORER_URL = "https://polygonscan.com";
 export const API_KEY = "ZuSU4sqkiZWPY9G2jJuWNSBu7WlqaVmK";
 export const BASE_ME_COLLECTION_URL =
